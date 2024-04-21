24892fd77bdd026d156957827e551c257d258418
SwippyA <shubhnkarswain16@gmail.com>
2024-04-21 20:44:03 +0530

    all routes for the given route name

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import {useState,useEffect} from "react"
+import { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom";
 import axios from "axios";
-import { useSelector, useDispatch } from 'react-redux'; 
-import { store } from "../Store/Store";
-import toast from "react-hot-toast"
- function  Home() {
+import { useSelector, useDispatch } from "react-redux";
+// import { store } from "../Store/Store";
+import toast from "react-hot-toast";
+// import { store } from "../Store/Store";
+function Home() {
   const dispatch = useDispatch();
-  const { isAuthenticated, user, token } = useSelector(state => state.auth);
+  const { isAuthenticated, user, token } = useSelector((state) => state.auth);
 
-  console.log(isAuthenticated)
-  console.log(user)
-  console.log(token)
+  // console.log(isAuthenticated);
+  // console.log(user);
+  // console.log(token);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -19,9 +20,11 @@ import toast from "react-hot-toast"
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:8000/api/v1/users/all_Videos");
+        const response = await axios.get(
+          "http://localhost:8000/api/v1/users/all_Videos"
+        );
         setData(response.data.data);
-        toast.success("get all videos")
+        toast.success("get all videos");
       } catch (error) {
         setError(error);
       } finally {
@@ -33,6 +36,7 @@ import toast from "react-hot-toast"
   }, []); // Empty dependency array means this effect will only run once, similar to componentDidMount
 
   if (loading) {
+    // toast.loading("Loading").charAt(1).remove();;
     return <div>Loading...</div>;
   }
 
@@ -59,8 +63,8 @@ import toast from "react-hot-toast"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-            clipRule="evenodd"
-            fillRule="evenodd"
+              clipRule="evenodd"
+              fillRule="evenodd"
               d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
             ></path>
           </svg>
@@ -74,7 +78,7 @@ import toast from "react-hot-toast"
             <ul className="space-y-2 font-medium">
               <li>
                 <NavLink
-                  to={"register"}
+                  to={""}
                   className="flex items-center p-2 text-purple-900 rounded-lg dark:text-white hover:bg-purple-700 dark:hover:bg-purple-700 group"
                 >
                   <svg
@@ -92,7 +96,8 @@ import toast from "react-hot-toast"
               </li>
               <li>
                 <NavLink
-                  href="#"
+                  to={"account"}
+                  // href="#"
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-purple-700 group"
                 >
                   <svg
@@ -111,7 +116,7 @@ import toast from "react-hot-toast"
 
               <li>
                 <NavLink
-                  href="#"
+                  to={"subscriptions"}
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-purple-700 group"
                 >
                   <svg
@@ -130,7 +135,7 @@ import toast from "react-hot-toast"
               </li>
               <li>
                 <NavLink
-                  href="#"
+                  to={"history"}
                   className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-purple-700 group"
                 >
                   <svg
@@ -142,51 +147,89 @@ import toast from "react-hot-toast"
                   >
                     <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
                   </svg>
-                  <span className="flex-1 ms-3 whitespace-nowrap">Libraries</span>
+                  <span className="flex-1 ms-3 whitespace-nowrap">
+                    Libraries
+                  </span>
                 </NavLink>
               </li>
-              <li>
-                <NavLink
-                  href="#"
-                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-purple-700 group"
-                >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 18 16"
+              {user ? (
+                ""
+              ) : (
+                <li>
+                  <NavLink
+                    to={"login"}
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-purple-700 group"
                   >
-                    <path
+                    <svg
+                      className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 18 16"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
+                      />
+                    </svg>
+                    <span className="flex-1 ms-3 whitespace-nowrap">
+                      Sign In
+                    </span>
+                  </NavLink>
+                </li>
+              )}
+              {user ? (
+                ""
+              ) : (
+                <li>
+                  <NavLink
+                    to={"register"}
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-purple-700 group"
+                  >
+                    <svg
+                      className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
+                      <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
+                      <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
+                    </svg>
+                    <span className="flex-1 ms-3 whitespace-nowrap">
+                      Sign Up
+                    </span>
+                  </NavLink>
+                </li>
+              )}
+              {user ? <li>
+                  <NavLink
+                    to={"login"}
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-purple-700 group"
+                  >
+                    <svg
+                      className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                      fill="none"
+                      viewBox="0 0 24 24"
                       stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
-                    />
-                  </svg>
-                  <span className="flex-1 ms-3 whitespace-nowrap">Sign In</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  href="#"
-                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-purple-700 group"
-                >
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
-                    <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
-                    <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
-                  </svg>
-                  <span className="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
-                </NavLink>
-              </li>
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
+                      />
+                    </svg>
+
+                    <span className="flex-1 ms-3 whitespace-nowrap">
+                      Logout
+                    </span>
+                  </NavLink>
+                </li> :""}
             </ul>
           </div>
         </aside>
