8aeb1d84b8277236d6b8d8328873dfb2fdbfb411
SwippyA <shubhnkarswain16@gmail.com>
2024-10-19 22:50:13 +0530

    all done

--- src/Home/Account.jsx
+++ src/Home/Account.jsx
@@ -31,7 +31,7 @@ function Account() {
 
   useEffect(() => {
     if (!isAuthenticated) {
-      toast.error("Please login");
+      toast.error("Please login to view your account");
       return;
     }
 
@@ -75,158 +75,167 @@ function Account() {
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   };
 
+  if (!isAuthenticated) {
+    // Show notification and prevent rendering content if not authenticated
+    return (
+      <>
+        <Header />
+        <div className="w-full h-fit flex flex-col mt-12 bg-gray-950 p-6">
+          <p className="text-white text-center">Please login to view your account</p>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
-    <Header/>
-    <div className="w-full h-fit flex flex-col mt-7 bg-gray-950 p-6">
-      <div className="w-full h-fit p-6 flex flex-col md:flex-row items-center justify-between gap-2">
-        <img
-          src={user.avatar}
-          alt=""
-          className="rounded-full w-44 h-44 md:w-60 md:h-60"
-        />
-        <div className="flex flex-col gap-4 md:flex-row md:items-center">
-          <div className="flex flex-col">
-            <h1 className="text-white text-5xl font-bold">{user?.fullName}</h1>
-            <p className="text-gray-500 text-sm">@{user?.username}</p>
-          </div>
-          <div className="flex gap-4">
-            <p className="text-gray-500 text-sm">
-              Subscriber: {data?.TotalSubscribers || 0}
-            </p>
-            <p className="text-gray-500 text-sm">
-              Total Videos: {data?.TotalVideos || 0}
-            </p>
-          </div>
-          <div className="flex gap-4">
-            <Link
-              to={"/editProfile"}
-              className="text-white text-sm px-7 py-2 rounded-2xl font-semibold bg-gray-800 hover:bg-purple-700 duration-200"
-            >
-              Edit Your Profile
-            </Link>
-            <Link
-              to={"/upload_video"}
-              className="text-white text-sm px-7 py-2 rounded-2xl duration-200 hover:bg-purple-700 font-semibold bg-gray-800"
-            >
-              Upload Video
-            </Link>
+      <Header />
+      <div className="w-full h-fit flex flex-col mt-7 bg-gray-950 p-6">
+        <div className="w-full h-fit p-6 flex flex-col md:flex-row items-center justify-between gap-2">
+          <img
+            src={user.avatar}
+            alt=""
+            className="rounded-full w-44 h-44 md:w-60 md:h-60"
+          />
+          <div className="flex flex-col gap-4 md:flex-row md:items-center">
+            <div className="flex flex-col">
+              <h1 className="text-white text-5xl font-bold">{user?.fullName}</h1>
+              <p className="text-gray-500 text-sm">@{user?.username}</p>
+            </div>
+            <div className="flex gap-4">
+              <p className="text-gray-500 text-sm">
+                Subscriber: {data?.TotalSubscribers || 0}
+              </p>
+              <p className="text-gray-500 text-sm">
+                Total Videos: {data?.TotalVideos || 0}
+              </p>
+            </div>
+            <div className="flex gap-4">
+              <Link
+                to={"/editProfile"}
+                className="text-white text-sm px-7 py-2 rounded-2xl font-semibold bg-gray-800 hover:bg-purple-700 duration-200"
+              >
+                Edit Your Profile
+              </Link>
+              <Link
+                to={"/upload_video"}
+                className="text-white text-sm px-7 py-2 rounded-2xl duration-200 hover:bg-purple-700 font-semibold bg-gray-800"
+              >
+                Upload Video
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="w-full h-fit flex flex-col gap-2">
-        <h1 className="text-white text-lg font-bold">Videos:</h1>
-        <hr />
-        <div className="w-full h-fit flex flex-wrap gap-7 p-4">
-          {videoData.length > 0 ? (
-            videoData.map((video) => (
-              <div key={video._id} className="p-3 relative">
-                <Link to={`/video/${video._id}`}>
-                  <img
-                    src={video.thumbnail}
-                    alt=""
-                    className="w-full md:w-[350px] h-[190px] hover:shadow-lg hover:shadow-purple-700 rounded-lg mb-3 cursor-pointer"
-                  />
-                </Link>
-                <div className="relative top-1 ">
-                  <button
-                    onClick={() => toggleMenu(video._id)}
-                    className="text-white absolute right-0 top-2 p-2"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 6.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12 12.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12 18.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                      />
-                    </svg>
-                  </button>
-
-                  {openMenuId === video._id && (
-                    <div className="absolute right-0 mt-8 w-48 bg-purple-700 rounded-md shadow-lg z-10">
-                      <ul className="py-1">
-                        {video.owner._id === user._id ? (
-                          <>
-                            <li>
-                              <Link
-                                to={`/update_video/${video._id}`}
-                                className="block px-4 py-2 text-sm text-white font-semibold hover:bg-gray-100 hover:text-black w-full text-left"
-                                onClick={() => {
-                                  console.log("Edit option clicked for", video._id);
-                                }}
-                              >
-                                Edit
-                              </Link>
-                            </li>
-                            <li>
-                              <button
-                                className="block px-4 py-2 text-sm text-white font-semibold hover:bg-gray-100 hover:text-black w-full text-left"
-                                onClick={() => deleteVideo(video._id)} // Pass the function reference
-                              >
-                                Delete
-                              </button>
-                            </li>
-                          </>
-                        ) : null}
-
-                        <li>
-                          <button
-                            className="block px-4 py-2 text-sm text-white font-semibold hover:bg-gray-100 hover:text-black w-full text-left"
-                            onClick={() => {
-                              console.log("Share option clicked for", video._id);
-                            }}
-                          >
-                            Share
-                          </button>
-                        </li>
-                      </ul>
-                    </div>
-                  )}
-                </div>
-                <span className="bg-black rounded-md text-white text-sm px-1 py-0.5 absolute bottom-[70px] right-5">
-                  {formatDuration(video.duration)}
-                </span>
-                <div className="flex items-center gap-3">
-                  <Link to={`/user/${video.owner._id}`}>
+        <div className="w-full h-fit flex flex-col gap-2">
+          <h1 className="text-white text-lg font-bold">Videos:</h1>
+          <hr />
+          <div className="w-full h-fit flex flex-wrap gap-7 p-4">
+            {videoData.length > 0 ? (
+              videoData.map((video) => (
+                <div key={video._id} className="p-3 relative">
+                  <Link to={`/video/${video._id}`}>
                     <img
-                      src={video.owner.avatar}
+                      src={video.thumbnail}
                       alt=""
-                      className="w-[40px] h-[40px] cursor-pointer rounded-full"
+                      className="w-full md:w-[350px] h-[190px] hover:shadow-lg hover:shadow-purple-700 rounded-lg mb-3 cursor-pointer"
                     />
                   </Link>
+                  <div className="relative top-1">
+                    <button
+                      onClick={() => toggleMenu(video._id)}
+                      className="text-white absolute right-0 top-2 p-2"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 6.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12 12.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12 18.75a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                        />
+                      </svg>
+                    </button>
 
-                  <h1 className="text-lg w-fit font-semibold text-white">
-                    {video.title}
-                  </h1>
-                </div>
-                <div className="text-white flex flex-col justify-center px-[52px]">
-                  <h1 className="text-sm w-fit font-semibold hover:text-purple-700 cursor-pointer">
-                    {video.owner.username}
-                  </h1>
-                  <div className="flex gap-2">
-                    <p className="text-gray-400 text-sm">
-                      Views: {video.views}
-                    </p>
-                    <p className="text-gray-400 text-sm">
-                      Date: {new Date(video.createdAt).toLocaleDateString()}
-                    </p>
+                    {openMenuId === video._id && (
+                      <div className="absolute right-0 mt-8 w-48 bg-purple-700 rounded-md shadow-lg z-10">
+                        <ul className="py-1">
+                          {video.owner._id === user._id ? (
+                            <>
+                              <li>
+                                <Link
+                                  to={`/update_video/${video._id}`}
+                                  className="block px-4 py-2 text-sm text-white font-semibold hover:bg-gray-100 hover:text-black w-full text-left"
+                                >
+                                  Edit
+                                </Link>
+                              </li>
+                              <li>
+                                <button
+                                  className="block px-4 py-2 text-sm text-white font-semibold hover:bg-gray-100 hover:text-black w-full text-left"
+                                  onClick={() => deleteVideo(video._id)}
+                                >
+                                  Delete
+                                </button>
+                              </li>
+                            </>
+                          ) : null}
+
+                          <li>
+                            <button
+                              className="block px-4 py-2 text-sm text-white font-semibold hover:bg-gray-100 hover:text-black w-full text-left"
+                              onClick={() => {
+                                console.log("Share option clicked for", video._id);
+                              }}
+                            >
+                              Share
+                            </button>
+                          </li>
+                        </ul>
+                      </div>
+                    )}
+                  </div>
+                  <span className="bg-black rounded-md text-white text-sm px-1 py-0.5 absolute bottom-[70px] right-5">
+                    {formatDuration(video.duration)}
+                  </span>
+                  <div className="flex items-center gap-3">
+                    <Link to={`/user/${video.owner._id}`}>
+                      <img
+                        src={video.owner.avatar}
+                        alt=""
+                        className="w-[40px] h-[40px] cursor-pointer rounded-full"
+                      />
+                    </Link>
+
+                    <h1 className="text-lg w-fit font-semibold text-white">
+                      {video.title}
+                    </h1>
+                  </div>
+                  <div className="text-white flex flex-col justify-center px-[52px]">
+                    <h1 className="text-sm w-fit font-semibold hover:text-purple-700 cursor-pointer">
+                      {video.owner.username}
+                    </h1>
+                    <div className="flex gap-2">
+                      <p className="text-gray-400 text-sm">
+                        Views: {video.views}
+                      </p>
+                      <p className="text-gray-400 text-sm">
+                        Date: {new Date(video.createdAt).toLocaleDateString()}
+                      </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))
-          ) : (
-            <p className="text-gray-500">No videos available</p>
-          )}
+              ))
+            ) : (
+              <p className="text-gray-500">No videos available</p>
+            )}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
--- src/Home/Subscription.jsx
+++ src/Home/Subscription.jsx
@@ -18,25 +18,32 @@ function Subscription() {
     // Fetch subscriptions data from API
     const fetchSubscriptions = async () => {
       try {
-        const response = await axios.get(
-          `https://backtube-1.onrender.com/api/v1/Subscription/c/${user._id}`,
-          {
-            headers: { Authorization: `Bearer ${accessToken}` },
-          }
-        );
-        setSubscriptions(response.data.data);
-        setLoading(false);
+        if (user && user._id) {
+          const response = await axios.get(
+            `https://backtube-1.onrender.com/api/v1/Subscription/c/${user._id}`,
+            {
+              headers: { Authorization: `Bearer ${accessToken}` },
+            }
+          );
+          setSubscriptions(response.data.data);
+        } else {
+          toast.error("User data is unavailable.");
+        }
       } catch (error) {
-        setError("NO  subscriptions found. ");
+        setError("No subscriptions found.");
         console.error("Error fetching subscriptions:", error);
-        setLoading(false);
+      } finally {
+        setLoading(false); // Ensure loading is set to false in all cases
       }
     };
 
     if (isAuthenticated) {
       fetchSubscriptions();
+    } else {
+      toast.error("Please login to view your subscriptions.");
+      setLoading(false); // Stop loading if not authenticated
     }
-  }, [user._id, accessToken, isAuthenticated]);
+  }, [user, accessToken, isAuthenticated]);
 
   if (loading) {
     return <Loading />;
@@ -52,58 +59,58 @@ function Subscription() {
 
   return (
     <>
-    <Header />
-    <div className="p-6 bg-gray-900 min-h-screen mt-10">
-      <h1 className="text-5xl text-purple-600 font-bold mb-8 text-center">
-        Your Subscriptions
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-        {subscriptions.length > 0 ? (
-          subscriptions.map((channel) => (
-            <div
-              key={channel.channelId}
-              className="relative group bg-gradient-to-t from-purple-900 via-gray-800 to-gray-900 p-6 rounded-lg shadow-lg transform hover:rotate-2 hover:scale-105 transition-transform duration-500 ease-in-out"
-            >
-              <div className="absolute inset-0 bg-gradient-to-br from-transparent to-black opacity-50 group-hover:opacity-75 transition-opacity duration-300 rounded-lg"></div>
-              <div className="relative flex flex-col items-center space-y-4">
-                <Link to={`/user/${channel.channelId}`}>
-                  <img
-                    src={channel.avatar}
-                    alt={`${channel.username} avatar`}
-                    className="w-20 h-20 rounded-full object-cover border-4 border-purple-500 hover:border-purple-300 transition-colors duration-300"
-                  />
-                </Link>
-                <h2 className="text-2xl font-bold text-white group-hover:text-purple-300 transition-colors duration-300">
-                  {channel.username}
-                </h2>
-              </div>
-              {channel.lastVideo && (
-                <div className="mt-6">
-                  <Link to={`/video/${channel.lastVideo._id}`}>
+      <Header />
+      <div className="p-6 bg-gray-900 min-h-screen mt-10">
+        <h1 className="text-5xl text-purple-600 font-bold mb-8 text-center">
+          Your Subscriptions
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          {subscriptions.length > 0 ? (
+            subscriptions.map((channel) => (
+              <div
+                key={channel.channelId}
+                className="relative group bg-gradient-to-t from-purple-900 via-gray-800 to-gray-900 p-6 rounded-lg shadow-lg transform hover:rotate-2 hover:scale-105 transition-transform duration-500 ease-in-out"
+              >
+                <div className="absolute inset-0 bg-gradient-to-br from-transparent to-black opacity-50 group-hover:opacity-75 transition-opacity duration-300 rounded-lg"></div>
+                <div className="relative flex flex-col items-center space-y-4">
+                  <Link to={`/user/${channel.channelId}`}>
                     <img
-                      src={channel.lastVideo.thumbnail}
-                      alt={channel.lastVideo.title}
-                      className="w-full h-48 object-cover rounded-md shadow-lg group-hover:opacity-90 transition-opacity duration-300"
+                      src={channel.avatar}
+                      alt={`${channel.username} avatar`}
+                      className="w-20 h-20 rounded-full object-cover border-4 border-purple-500 hover:border-purple-300 transition-colors duration-300"
                     />
                   </Link>
-                  <h3 className="mt-4 text-lg font-semibold text-gray-300">
-                    {channel.lastVideo.title}
-                  </h3>
-                  <p className="text-sm text-gray-400">
-                    {channel.lastVideo.views} views &middot;{" "}
-                    {new Date(
-                      channel.lastVideo.uploadDate
-                    ).toLocaleDateString()}
-                  </p>
+                  <h2 className="text-2xl font-bold text-white group-hover:text-purple-300 transition-colors duration-300">
+                    {channel.username}
+                  </h2>
                 </div>
-              )}
-            </div>
-          ))
-        ) : (
-          <div className="text-center text-white">No subscriptions found.</div>
-        )}
+                {channel.lastVideo && (
+                  <div className="mt-6">
+                    <Link to={`/video/${channel.lastVideo._id}`}>
+                      <img
+                        src={channel.lastVideo.thumbnail}
+                        alt={channel.lastVideo.title}
+                        className="w-full h-48 object-cover rounded-md shadow-lg group-hover:opacity-90 transition-opacity duration-300"
+                      />
+                    </Link>
+                    <h3 className="mt-4 text-lg font-semibold text-gray-300">
+                      {channel.lastVideo.title}
+                    </h3>
+                    <p className="text-sm text-gray-400">
+                      {channel.lastVideo.views} views &middot;{" "}
+                      {new Date(
+                        channel.lastVideo.uploadDate
+                      ).toLocaleDateString()}
+                    </p>
+                  </div>
+                )}
+              </div>
+            ))
+          ) : (
+            <div className="text-center text-white">No subscriptions found.</div>
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 }
